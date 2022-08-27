3be1c8c103fa189dd69b46fb194f4c45ff886f17
Magnus Myreen <magnus.myreen@cl.cam.ac.uk>
2022-08-27 23:29:13 +0200

    Add EditorModel

--- java/EditorModel.java
+++ java/EditorModel.java
@@ -7,13 +7,18 @@ public class EditorModel implements GridModel {
     private GOLState golState;
     private Grid2DBackground bg;
 
-    public EditorModel(String str) {
+    public EditorModel(String rle) {
         golState = new GOLState();
         bg = new Grid2DBackground();
     }
 
     public Color cell(int x, int y) {
-        return bg.cell(x,y);
+        BoolExp b = cell(int x, int y);
+        if (b instanceof BoolFalse) {
+            return bg.cell(x,y);
+        } else {
+            return Color.WHITE;
+        }
     }
 
 }
