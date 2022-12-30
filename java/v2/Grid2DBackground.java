dc402f83d5fbd908935fad514cb902e866935d18
Magnus Myreen <magnus.myreen@cl.cam.ac.uk>
2022-12-30 12:06:12 +0700

    Mark entry/exit boxes

--- java/v2/Grid2DBackground.java
+++ java/v2/Grid2DBackground.java
@@ -30,7 +30,19 @@ public class Grid2DBackground implements GridModel {
     private final Color bgDark = new Color(40,40,40);
     private final Color bgVeryDark = new Color(30,30,30);
 
+    private boolean inBox(int i, int j, int x, int y, int w, int h) {
+        return (x <= i && i < x+w && y <= j && j < y+h);
+    }
+
     public Color cell(int i, int j) {
+        if (inBox(i,j,0,-2,5,5)) { return new Color(120,20,20); }       // e in
+        if (inBox(i,j,0+150,-2,5,5)) { return new Color(120,20,20); }   // e out
+        if (inBox(i,j,145,2,5,5)) { return new Color(120,20,20); }      // w in
+        if (inBox(i,j,145-150,2,5,5)) { return new Color(120,20,20); }  // w out
+        if (inBox(i,j,71,75,5,17)) { return new Color(120,20,20); }     // n in
+        if (inBox(i,j,71,75-150,5,17)) { return new Color(120,20,20); } // n out
+        if (inBox(i,j,74,62-150,5,14)) { return new Color(20,120,20); } // s in
+        if (inBox(i,j,74,62,5,14)) { return new Color(20,120,20); }     // s out
         if (isGatePos(i,j)) {
             return bgLight;
         }
